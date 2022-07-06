a118c6e1ecd0f9be7b9ca6b47e13160697f1329c
SandMl3 <sandramartinlop@gmail.com>
2022-07-06 10:02:59 +0200

    Add js functionalities

--- main.js
+++ main.js
@@ -1,16 +1,45 @@
 'use strict';
 
-/* El juego consiste en que la aplicación genera un número al azar entre 1 y 100 y la jugadora tiene que
-adivinarlo. El juego da pistas sobre si el número que prueba es demasiado alto o bajo, y va contabilizando el
-número de intentos. Hasta que al final la jugadora acierta el número. Si la jugadora quiere adivinar otro
-número, deberá volver a cargar la página. */
+//Pasos a seguir:
+//--1.Guardamos en variables los elementos HTML que vamos a necesitar: input, botón, textos(pista y contador de intentos).
+//--2. Añadimos la función que genera números aleatorios y comprobamos que funciona correctamente. Guardamos su retorno en una constante para trabajar con el número aleatorio generado.
+//--3.Añadimos el evento click al botón y definimos las funciones que contendrá: 
+//------a.Sumar intento al contador.
+//------b.Comprobar si el número introducido está dentro del rango permitido.
+//------c.Comprobar si el número es igual al random o no y mostrar pistas en función de ello.
+//------d.Inicializar el campo del input.
 
 
-console.log('Esto funciona');
 
-function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-};
+const inputUserNumber = document.querySelector('.main__form__userNumber');
+const buttonForm = document.querySelector('.main__form__button');
+const textClue = document.querySelector('.main__clue');
+const textTries = document.querySelector('.main__triesNumber');
+let numberOfTries = 0;
 
+const getRandomNumber = max => Math.ceil(Math.random() * max);
 const randonNumber = getRandomNumber(100);
+
 console.log(`Mi número aleatorio es ${randonNumber}`);
+
+
+
+const checkNumber = userNumber => parseInt(userNumber) === randonNumber ?textClue.innerHTML = "¡¡¡Has ganado, campeona!!! Refresca la página para comenzar un nuevo reto." : parseInt(userNumber) < randonNumber ?textClue.innerHTML = "Demasiado bajo.": textClue.innerHTML = "Demasiado alto.";
+
+const validateUserNumber = userNumber => userNumber < 1 || userNumber > 100 ? textClue.innerHTML = "El número debe estar entre 1 y 100": checkNumber(userNumber);
+
+const triesCounter = (numberOfTries) => textTries.innerHTML = `Número de intentos: ${numberOfTries}`; 
+
+const initInputNumber = () => inputUserNumber.value = "";
+
+const handlerFunction = (event) => {
+    event.preventDefault();
+    const userNumber = inputUserNumber.value;
+    numberOfTries += 1;
+
+    triesCounter(numberOfTries);
+    validateUserNumber(userNumber);
+    initInputNumber();
+}
+
+buttonForm.addEventListener('click', handlerFunction);

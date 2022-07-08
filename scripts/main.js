73dcb5c679ef2b07aa96b3ec5de5f26ddb5b75ec
SandMl3 <sandramartinlop@gmail.com>
2022-07-08 16:53:56 +0200

    Add new functionality like a reset button and different notifications if the user enters a number greater or less than random number

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,8 @@ const inputUserNumber = document.querySelector('.main__form__userNumber');
 const buttonForm = document.querySelector('.main__form__button');
 const textClue = document.querySelector('.main__clue');
 const textTries = document.querySelector('.main__triesNumber');
-const form = document.querySelector('.main__form')
+const form = document.querySelector('.main__form');
+const resetButton = document.querySelector('.main__form__button--reset');
 let numberOfTries = 0;
 
 const getRandomNumber = max => Math.ceil(Math.random() * max);
@@ -24,21 +25,38 @@ const randonNumber = getRandomNumber(100);
 
 console.log(`Mi número aleatorio es ${randonNumber}`);
 
-const checkNumber = userNumber => userNumber === randonNumber 
+const checkNumber = userNumber => {
+  if (textClue.classList.contains('error')) {
+    textClue.classList.remove('error');
+  }
+  userNumber === randonNumber 
   ?includeHTML('<span class="main__clue--bold">¡¡¡Has ganado, campeona!!!</span> Refresca la página para comenzar un nuevo reto.', textClue) 
   : userNumber < randonNumber 
   ?includeHTML('Demasiado bajo.', textClue) 
   :includeHTML('Demasiado alto.', textClue);
+}
 
-const validateUserNumber = (userNumber) =>!userNumber
-  ?includeHTML('Debe introducir un número.',textClue) 
-  :userNumber < 1 || userNumber > 100 
-  ? includeHTML('El número debe estar entre 1 y 100', textClue)
-  : checkNumber(userNumber);
+const validateUserNumber = (userNumber) => {
+  if (!userNumber && userNumber !== 0) {
+    includeHTML('Debe introducir un número.',textClue);
+    textClue.classList.add('error');
+  } else {
+    userNumber < 1 || userNumber > 100 
+    ? numberOutOfRange(userNumber)
+    : checkNumber(userNumber);
+  }
+};
 
 const includeHTML = (string, element) => {
   element.innerHTML = string;
 };
+
+const numberOutOfRange = (userNumber) => {
+  textClue.classList.add('error');
+  userNumber < 1 
+    ?includeHTML('Has introducido un número menor de 1.<span class="explanation"> El número debe estar comprendido entre 1 y 100.</span>', textClue)
+    :includeHTML('Has introducido un número mayor de 100.<span class="explanation"> El número debe estar comprendido entre 1 y 100.</span>', textClue);
+}
 
 const triesCounter = () => {
   numberOfTries++;
@@ -65,3 +83,24 @@ const avoidRefreshing = (event) => {
 
 buttonForm.addEventListener('click', handlerFunction);
 inputUserNumber.addEventListener('keypress', avoidRefreshing);
+
+const initInput = () => inputUserNumber.value = "";
+const initTries = () => {
+  numberOfTries= 0
+  includeHTML(`Número de intentos: ${numberOfTries}`, textTries);
+};
+const initTextClue = () => {
+  if (textClue.classList.contains('error')) {
+    textClue.classList.remove('error');
+  };
+  includeHTML(`Pista: <span class="main__clue--capitalize">escribe</span> el número y dale a <span class="main__clue--capitalize">prueba</span>`, textClue);
+};
+
+const resetHandlerFunction = (event) => {
+  event.preventDefault();
+  initInput();
+  initTries();
+  initTextClue();
+};
+
+resetButton.addEventListener('click', resetHandlerFunction);
